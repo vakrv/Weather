d5b251228d66ef1e0dfab6d103eb7b027bc087d1
Artem Vakurov <temavakurov2@icloud.com>
2022-12-25 15:03:56 +0300

    добавил статический, неизменяемый список городов

---
+++ app/src/main/java/weather/view/MainActivity.kt
@@ -0,0 +1,18 @@
+package weather.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.weather.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
