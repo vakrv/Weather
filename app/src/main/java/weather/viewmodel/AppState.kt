1dcbdcc25780e1c9d72e8434f1a00db36fe4e050
Artem Vakurov <temavakurov2@icloud.com>
2023-01-11 12:00:02 +0300

    hw6

--- app/src/main/java/weather/viewmodel/AppState.kt
+++ app/src/main/java/weather/viewmodel/AppState.kt
@@ -7,5 +7,6 @@ sealed class AppState {
     data class Success(val weatherData: Weather) : AppState()
     data class Error(val error: Throwable) : AppState()
     object Loading : AppState()
+
 }
 
